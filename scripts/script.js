c7fe7957873b8b68758e4a1974dc1125817d941f
eliran075 <84390133+eliran075@users.noreply.github.com>
2021-09-28 19:38:02 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,21 @@
-// JavaScript Document
+var openmenu = document.querySelector(".openmenu");
+var closemenu = document.querySelector(".closemenu");
+var menulinks = document.querySelector(".menu li");
+var menu = document.querySelector(".menu")
+
+openmenu.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    // checkt of het menu open staat, in het geval haalt ie het weg
+    if (menu.classList.contains(".showMenu")) {
+        menu.classList.remove(".showMenu");
+        closemenu.style.display = "none";
+        menu.style.display = "none";
+    }
+    // anders voegt hij de code toe om het menu te vertonen
+    else {
+        menu.classList.add(".showMenu");
+        closemenu.style.display = "block";
+        menu.style.display = "block";
+    }
+}
